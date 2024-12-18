cf18ca920d733376b6832f00ac346f6e9b297a7e
Framirez <framireze@eservicios.indra.es>
2024-12-18 16:04:15 +0100

    webpack html plugin installed and configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import path from "path"
+import url from "url"
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export default {
   entry: ["./src/index.js"],
   module: {
@@ -8,5 +14,17 @@ export default {
         loader: "babel-loader"
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      scriptLoading: "blocking"
+    })
+  ],
+  devServer: {
+    port: 2244,
+    static: path.join(__dirname, "src"),
+    hot: true
+  },
 }
