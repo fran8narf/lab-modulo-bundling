c815406c2553515339cda7bd7b4ab8926100900a
Framirez <framireze@eservicios.indra.es>
2024-12-19 17:16:44 +0100

    1st react component created | index.jsx root render created

---
+++ src/helloWorldComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const HellowWorldComponent = () => {
+  const greetingsSource = 'desde React!!'
+  return(
+    <div>
+      <h1>Hola Lemoncoders desde {greetingsSource}</h1>
+    </div>
+  )
+}
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import "./styles.scss";
-
-const userName = "Juan Doe desde el fichero TS";
-const hello = `Hello, ${userName}`;
-
-console.log(hello);
