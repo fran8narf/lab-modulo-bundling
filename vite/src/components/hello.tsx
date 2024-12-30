2baad7d4b0f5cdd72234469dade548ead14132cd
fran8narf <srlk_doblev@hotmail.com>
2024-12-30 17:32:15 +0100

    saving env vars in config file

--- vite/src/components/hello.tsx
+++ vite/src/components/hello.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
+import config from "../config";
 
 export const HelloComponent: FC = () => {
     return (
     <>
         <h2>React ready to use!</h2>
-        <p>Env vars working: {import.meta.env.VITE_API_BASE} </p>
+        <p>Env vars working: {config.API_BASE} </p>
     </>
     )
 }
