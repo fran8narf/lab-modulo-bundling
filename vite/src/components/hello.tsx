a61b5d8084dc2de2fc064ef3bbc48cf57604efd0
fran8narf <srlk_doblev@hotmail.com>
2024-12-30 17:27:36 +0100

    env vars config and working

--- vite/src/components/hello.tsx
+++ vite/src/components/hello.tsx
@@ -1,5 +1,10 @@
 import { FC } from "react";
 
 export const HelloComponent: FC = () => {
-    return <h2>React ready to use!</h2>
+    return (
+    <>
+        <h2>React ready to use!</h2>
+        <p>Env vars working: {import.meta.env.VITE_API_BASE} </p>
+    </>
+    )
 }
